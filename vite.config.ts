429d23a815c28e35f312493662fb5d9d1ae6ac06
Daosolary <adsubagi@gmail.com>
2025-12-21 15:38:22 +0000

    SEO google

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,27 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import prerender from 'vite-plugin-prerender'
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5175,
-    open: true,
-  },
+  plugins: [
+    react(),
+    prerender({
+      routes: [
+        '/',
+        '/sobre',
+        '/servicos',
+        '/portfolio',
+        '/contato',
+        '/blog',
+        '/faq',
+        '/depoimentos',
+        '/servicos/desenvolvimento-web',
+        '/servicos/desenvolvimento-mobile',
+        '/servicos/consultoria',
+      ],
+    }),
+  ],
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
-      },
-    },
   },
-});
-
-
-
-
-
-
-
+})
