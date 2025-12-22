d4adfca86b017a78cd4d3bd7865a123c69c45bf5
Daosolary <adsubagi@gmail.com>
2025-12-22 13:28:15 +0000

    teste de SEO

--- vite.config.ts
+++ vite.config.ts
@@ -3,26 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5175,
-    open: true,
-  },
   build: {
     outDir: 'dist',
-    sourcemap: true,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-        },
-      },
-    },
   },
 });
-
-
-
-
-
-
-
